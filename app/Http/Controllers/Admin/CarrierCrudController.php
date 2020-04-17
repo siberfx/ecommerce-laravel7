60650d4be90e13d24045aa09a69c1819b182f9a5
Selim Gormus (dell) <info@siberfx.com>
2020-04-17 19:19:56 +0300

    added Carrier crud and request

--- app/Http/Controllers/Admin/CarrierCrudController.php
+++ app/Http/Controllers/Admin/CarrierCrudController.php
@@ -4,44 +4,103 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Requests\CarrierRequest;
 use Backpack\CRUD\app\Http\Controllers\CrudController;
+use Backpack\CRUD\app\Http\Controllers\Operations\CreateOperation;
+use Backpack\CRUD\app\Http\Controllers\Operations\DeleteOperation;
+use Backpack\CRUD\app\Http\Controllers\Operations\ListOperation;
+use Backpack\CRUD\app\Http\Controllers\Operations\ShowOperation;
+use Backpack\CRUD\app\Http\Controllers\Operations\UpdateOperation;
+use Backpack\CRUD\app\Library\CrudPanel\CrudPanel;
 use Backpack\CRUD\app\Library\CrudPanel\CrudPanelFacade as CRUD;
 
 /**
  * Class CarrierCrudController
  * @package App\Http\Controllers\Admin
- * @property-read \Backpack\CRUD\app\Library\CrudPanel\CrudPanel $crud
+ * @property-read CrudPanel $crud
  */
 class CarrierCrudController extends CrudController
 {
-    use \Backpack\CRUD\app\Http\Controllers\Operations\ListOperation;
-    use \Backpack\CRUD\app\Http\Controllers\Operations\CreateOperation;
-    use \Backpack\CRUD\app\Http\Controllers\Operations\UpdateOperation;
-    use \Backpack\CRUD\app\Http\Controllers\Operations\DeleteOperation;
-    use \Backpack\CRUD\app\Http\Controllers\Operations\ShowOperation;
+    use ListOperation;
+    use CreateOperation;
+    use UpdateOperation;
+    use DeleteOperation;
+    use ShowOperation;
 
     public function setup()
     {
         $this->crud->setModel('App\Models\Carrier');
-        $this->crud->setRoute(config('backpack.base.route_prefix') . '/carrier');
+        $this->crud->setRoute(config('backpack.base.route_prefix') . '/carriers');
         $this->crud->setEntityNameStrings('carrier', 'carriers');
+
+        $this->crud->addColumns(
+            $this->getColumns()
+        );
+        $this->crud->addFields(
+            $this->getFields()
+        );
+
     }
 
     protected function setupListOperation()
     {
-        // TODO: remove setFromDb() and manually define Columns, maybe Filters
-        $this->crud->setFromDb();
     }
 
     protected function setupCreateOperation()
     {
         $this->crud->setValidation(CarrierRequest::class);
-
-        // TODO: remove setFromDb() and manually define Fields
-        $this->crud->setFromDb();
     }
 
     protected function setupUpdateOperation()
     {
-        $this->setupCreateOperation();
+        $this->crud->setValidation(CarrierRequest::class);
+
+    }
+
+    /**
+     * @return array
+     */
+    private function getColumns()
+    {
+        return [
+            [
+                'name'  => 'name',
+                'label' => trans('category.name'),
+            ]
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    private function getFields()
+    {
+        return [
+            [
+                'name'  => 'name',
+                'label' => trans('carrier.name'),
+                'type'  => 'text',
+            ],
+            [
+                'name'       => 'price',
+                'label'      => trans('carrier.price'),
+                'type'       => 'number',
+                'attributes' => [
+                    'step' => 'any'
+                ]
+            ],
+            [
+                'name'  => 'delivery_text',
+                'label' => trans('carrier.delivery_text'),
+                'type'  => 'text',
+            ],
+            [
+                'name'    => "logo",
+                'label'   => trans('carrier.logo'),
+                'type'    => 'image',
+                'upload'  => true,
+                'crop'    => false,
+                'default' => 'default.png',
+                'prefix'  => 'uploads/carriers/'
+            ]
+        ];
     }
 }
--- app/Http/Requests/CarrierRequest.php
+++ app/Http/Requests/CarrierRequest.php
@@ -26,7 +26,8 @@ class CarrierRequest extends FormRequest
     public function rules()
     {
         return [
-            // 'name' => 'required|min:5|max:255'
+            'name'  => 'required',
+            'price' => 'required|numeric'
         ];
     }
 
