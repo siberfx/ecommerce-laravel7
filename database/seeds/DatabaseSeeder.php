0a201109b5adbcc5a3b9adaf22cadca4a636a8e4
Selim Gormus (dell) <info@siberfx.com>
2020-04-17 20:11:41 +0300

    important seeders are applied

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
+        $this->call(SpecificPriceDiscountTypesTableSeeder::class);
+        $this->call(CartRuleDiscountTypesTableSeeder::class);
     }
 }
