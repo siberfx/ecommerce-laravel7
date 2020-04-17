cea9ad86bdaf023af6a975c46f1b9a2ba6ac7bdd
Selim Gormus (dell) <info@siberfx.com>
2020-04-17 19:15:53 +0300

    added Attribute Set crud and request

--- app/Http/Controllers/Admin/AttributeSetCrudController.php
+++ app/Http/Controllers/Admin/AttributeSetCrudController.php
@@ -4,44 +4,110 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Requests\AttributeSetRequest;
 use Backpack\CRUD\app\Http\Controllers\CrudController;
+use Backpack\CRUD\app\Http\Controllers\Operations\CreateOperation;
+use Backpack\CRUD\app\Http\Controllers\Operations\DeleteOperation;
+use Backpack\CRUD\app\Http\Controllers\Operations\ListOperation;
+use Backpack\CRUD\app\Http\Controllers\Operations\ShowOperation;
+use Backpack\CRUD\app\Http\Controllers\Operations\UpdateOperation;
+use Backpack\CRUD\app\Library\CrudPanel\CrudPanel;
 use Backpack\CRUD\app\Library\CrudPanel\CrudPanelFacade as CRUD;
 
 /**
  * Class AttributeSetCrudController
  * @package App\Http\Controllers\Admin
- * @property-read \Backpack\CRUD\app\Library\CrudPanel\CrudPanel $crud
+ * @property-read CrudPanel $crud
  */
 class AttributeSetCrudController extends CrudController
 {
-    use \Backpack\CRUD\app\Http\Controllers\Operations\ListOperation;
-    use \Backpack\CRUD\app\Http\Controllers\Operations\CreateOperation;
-    use \Backpack\CRUD\app\Http\Controllers\Operations\UpdateOperation;
-    use \Backpack\CRUD\app\Http\Controllers\Operations\DeleteOperation;
-    use \Backpack\CRUD\app\Http\Controllers\Operations\ShowOperation;
+    use ListOperation;
+    use CreateOperation;
+    use UpdateOperation;
+    use DeleteOperation;
+    use ShowOperation;
 
     public function setup()
     {
         $this->crud->setModel('App\Models\AttributeSet');
-        $this->crud->setRoute(config('backpack.base.route_prefix') . '/attributeset');
-        $this->crud->setEntityNameStrings('attributeset', 'attribute_sets');
+        $this->crud->setRoute(config('backpack.base.route_prefix') . '/attributes-sets');
+        $this->crud->setEntityNameStrings('attribute set', 'attribute sets');
+
+        $this->crud->addColumns(
+            $this->getColumns()
+        );
+        $this->crud->addFields(
+            $this->getFields()
+        );
+
     }
+
 
     protected function setupListOperation()
     {
-        // TODO: remove setFromDb() and manually define Columns, maybe Filters
-        $this->crud->setFromDb();
     }
 
     protected function setupCreateOperation()
     {
         $this->crud->setValidation(AttributeSetRequest::class);
-
-        // TODO: remove setFromDb() and manually define Fields
-        $this->crud->setFromDb();
     }
 
     protected function setupUpdateOperation()
     {
-        $this->setupCreateOperation();
+        $this->crud->setValidation(AttributeSetRequest::class);
+
+    }
+
+    /**
+     * @return array
+     */
+    private function getColumns()
+    {
+        return [
+            [
+                'name'  => 'name',
+                'label' => trans('attribute.name'),
+            ]
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    private function getFields()
+    {
+        return [
+            [
+                'name'      => 'name',
+                'label'     => trans('attribute.name'),
+                'type'      => 'text',
+            ],
+            [
+                'type'      => 'select2_multiple',
+                'label'     => trans('attribute.attributes'),
+                'name'      => 'attributes',
+                'entity'    => 'attributes',
+                'attribute' => 'name',
+                'model'     => "App\Models\Attribute",
+                'pivot'     => true,
+            ]
+        ];
+    }
+
+
+    public function ajaxGetAttributesBySetId(Request $request, Attribute $attribute)
+    {
+        // Init old as an empty array
+        $old = [];
+
+        // Set old inputs as array from $request
+        if (isset($request->old)) {
+            $old = json_decode($request->old, true);
+        }
+
+        // Get attributes with values by set id
+        $attributes = $attribute->with('values')->whereHas('sets', function ($q) use ($request) {
+            $q->where('id', $request->setId);
+        })->get();
+
+        return view('renders.product_attributes', compact('attributes', 'old'));
     }
 }
--- app/Http/Requests/AttributeSetRequest.php
+++ app/Http/Requests/AttributeSetRequest.php
@@ -26,7 +26,8 @@ class AttributeSetRequest extends FormRequest
     public function rules()
     {
         return [
-            // 'name' => 'required|min:5|max:255'
+            'name'       => 'required',
+            'attributes' => 'required',
         ];
     }
 
